ca3a6ccf96fb4d40a9ac1f54f39659106801905f
vaishu vollala <vaishu vollala@LAPTOP-U09TFG76>
2022-02-07 11:14:12 +0530

    commit

---
+++ com.java/src/com/java/Ex2.java
@@ -0,0 +1,34 @@
+package com.java;
+
+public class Ex2 {
+	public static void main(String[] args) {
+		//Write a program to print student information using student information using java variables and literals
+		//variable declaration
+		
+		int studentId;
+		String studentName;
+		float studentMarks;
+		String studentAddress;
+		
+		//Variable initialization
+		
+		studentId=3020;
+		studentName="Vishwa";
+		studentMarks=98f;
+		studentAddress="Warangal";
+		
+		System.out.print("studentId: ");
+		System.out.println(studentId);
+		
+		System.out.print("studentName: ");
+		System.out.println(studentName);
+		
+		System.out.print("studentMarks: ");
+		System.out.println(studentMarks);
+		
+		System.out.print("studentAddress: ");
+		System.out.println(studentAddress);
+		
+	}
+
+}
